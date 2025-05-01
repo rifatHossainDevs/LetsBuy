691ef7e032f1f2dac837e23c1699786458285b3f
Rifat Hossain <mdrifathossain472@gmail.com>
2025-05-02 03:13:49 +0600

    Added BaseFragment,

--- app/src/main/java/com/esports/letsbuy/Views/dashboard/DashBoardFragment.kt
+++ app/src/main/java/com/esports/letsbuy/Views/dashboard/DashBoardFragment.kt
@@ -1,12 +1,5 @@
 package com.esports.letsbuy.Views.dashboard
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.viewbinding.ViewBinding
-import com.esports.letsbuy.R
 import com.esports.letsbuy.base.BaseFragment
 import com.esports.letsbuy.databinding.FragmentDashBoardBinding
 
